693882d225b3f1a25a3c4c2509acd396d1fd48f7
Luke Nodarishvili <132289854+LukeNodarishvili@users.noreply.github.com>
2024-11-04 19:24:08 +0000

    Add files via upload

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,94 @@
+import bandSiteAPI from "./band-site-api.js";
+
+const bandsiteAPI = new bandSiteAPI();
+
+const getData = async () => {
+  console.log(bandSiteAPI);
+  let showsList = await bandsiteAPI.getShows();
+
+  displayShows(showsList);
+};
+
+const targetHTML = document.getElementById("showsDiv");
+
+function displayShows(showsList) {
+  for (let i = 0; i < showsList.length; i++) {
+    let createdArticle = document.createElement("article"); //makes the main article box, and adds the on-click system
+    createdArticle.classList.add("individual-show");
+
+    // Highlighting section
+    document.addEventListener("click", function (event) {
+      if (!createdArticle.contains(event.target)) {
+        createdArticle.classList.remove("individual-show--clicked");
+      }
+    });
+    createdArticle.onclick = function () {
+      createdArticle.classList.toggle("individual-show--clicked");
+    };
+
+    //1St Div (Reuse as needed) Date
+    let dateHTML = document.createElement("div"); //makes the 1st div
+    dateHTML.classList.add("individual-show__data");
+
+    let dateName = document.createElement("p"); //makes the P tag to put the string into
+    dateName.innerText = "DATE";
+    dateName.classList.add("individual-show__label");
+    dateHTML.appendChild(dateName); // adds the "DATE" to the div
+
+    let dateData = new Date(showsList[i].date);
+    let dateDataHTML = document.createElement("p");
+    dateDataHTML.innerText = dateData.toLocaleString("en-US", {
+      weekday: "short",
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+    }); //puts the data (text) into the P tag
+    dateDataHTML.classList.add("individual-show__data__date");
+    dateHTML.appendChild(dateDataHTML); // adds the data (text) to the div
+
+    //2nd Div (Reuse as needed) Venue
+    let venueHTML = document.createElement("div"); //makes the 1st div
+    venueHTML.classList.add("individual-show__data");
+
+    let venueName = document.createElement("p"); //makes the P tag to put the string into
+    venueName.innerText = "VENUE";
+    venueName.classList.add("individual-show__label");
+    venueHTML.appendChild(venueName); // adds the "VENUE" to the div
+
+    let venueData = showsList[i].place;
+    let venueDataHTML = document.createElement("p");
+    venueDataHTML.innerText = venueData; //puts the data (text) into the P tag
+    venueDataHTML.classList.add("individual-show__data__other");
+    venueHTML.appendChild(venueDataHTML); // adds the data (text) to the div
+
+    //3rd Div (Reuse as needed) Loc
+    let LocationHTML = document.createElement("div"); //makes the 1st div
+    LocationHTML.classList.add("individual-show__data");
+
+    let LocationName = document.createElement("p"); //makes the P tag to put the string into
+    LocationName.innerText = "LOCATION";
+    LocationName.classList.add("individual-show__label");
+    LocationHTML.appendChild(LocationName); // adds the "LOCATION" to the div
+
+    let LocationData = showsList[i].location;
+    let LocationDataHTML = document.createElement("p");
+    LocationDataHTML.innerText = LocationData; //puts the data (text) into the P tag
+    LocationDataHTML.classList.add("individual-show__data__other");
+    LocationHTML.appendChild(LocationDataHTML); // adds the data (text) to the div
+
+    //button maker
+    let buttonHTML = document.createElement("button");
+    buttonHTML.classList.add("individual-show__buy-tickets");
+    buttonHTML.innerText = "BUY TICKETS";
+
+    // Add all elements together (ORDER IS IMPORTANT, FUTURE PROGRAMER)
+    createdArticle.appendChild(dateHTML);
+    createdArticle.appendChild(venueHTML);
+    createdArticle.appendChild(LocationHTML);
+    createdArticle.appendChild(buttonHTML);
+
+    // Final product is added
+    targetHTML.appendChild(createdArticle);
+  }
+}
+getData();
---
+++ scripts/index-page.js
@@ -0,0 +1,90 @@
+import bandSiteAPI from "./band-site-api.js";
+
+const bandsiteAPI = new bandSiteAPI();
+
+const getData = async () => {
+  console.log(bandSiteAPI);
+  let userComments = await bandsiteAPI.getComments();
+  userComments.sort((a, b) => b.timestamp - a.timestamp);
+  loadComments(userComments);
+};
+
+document.getElementById("form-all").addEventListener("submit", displayComments);
+
+async function displayComments(event) {
+  event.preventDefault();
+
+  let nameText = document.getElementById("form-name").value;
+
+  let commentText = document.getElementById("form-comment").value;
+
+  let newComment = {
+    name: nameText,
+    comment: commentText,
+  };
+  await bandsiteAPI.postComments(newComment); //This is the current error
+
+  //userComments.unshift(newComment);
+  getData();
+  //loadComments();
+
+  document.getElementById("form-all").reset();
+}
+
+function loadComments(userComments) {
+  const targetHTML = document.getElementById("commentSection");
+  targetHTML.innerHTML = "";
+  for (let i = 0; i < userComments.length; i++) {
+    let createdArticle = document.createElement("article"); //makes the main article box
+    createdArticle.classList.add("comment"); // adds CSS class to the box
+    //in logic we add the image to this, THEN after add the next line:
+
+    let articleHTML = document.createElement("div"); //makes the text div
+    articleHTML.classList.add("comment__main");
+    //in logic once the text div is made we then put in the header__date div in:
+
+    let headerdateDiv = document.createElement("div"); //makes the username and date div
+    headerdateDiv.classList.add("comment__header");
+    //then only after do we add the comment text
+
+    //Making variables and HTML for adding to the above Divs
+
+    let nameText = userComments[i].name; //finds the string
+    let nameHTML = document.createElement("p"); //makes the P tag to put the string into
+    nameHTML.innerText = nameText; //puts the string into the P tag inside the "box" so it can be used later.
+
+    let dateText = new Date(userComments[i].timestamp);
+    let dateHTML = document.createElement("p");
+    dateHTML.classList.add("comment__header--date");
+    dateHTML.innerText = dateText.toLocaleString("en-US", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    });
+
+    let commentText = userComments[i].comment;
+    let commentHTML = document.createElement("p");
+    commentHTML.innerText = commentText;
+
+    let profilepPictureSrc = userComments[i].profilepPicture;
+    let profilepPictureHTML = document.createElement("img"); //if profilepPicture is blank it will simply use the backup
+    if (profilepPictureSrc == "undefined") {
+      profilepPictureHTML.setAttribute("src", profilepPictureSrc); //if PFP is missing this will not activate. putting a placeholder instead
+    }
+    profilepPictureHTML.classList.add("comment__image");
+    // musti be modified ... somehow?
+
+    //Adding the Divs and P tags together and then formating
+    headerdateDiv.appendChild(nameHTML); //adds text to Div 4 in two parts for CSS reasons
+    headerdateDiv.appendChild(dateHTML);
+
+    articleHTML.appendChild(headerdateDiv);
+    articleHTML.appendChild(commentHTML);
+
+    createdArticle.appendChild(profilepPictureHTML);
+    createdArticle.appendChild(articleHTML);
+    targetHTML.appendChild(createdArticle);
+  }
+}
+
+getData();
